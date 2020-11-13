f399124b046f0f617f8ab90163ceec8a95965671
iamstanisla <dmitry@mail.com>
2020-11-13 07:54:45 +0300

    add base for logging in file .log, folder is app/logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
+from logging.handlers import RotatingFileHandler
+import logging
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from pony.flask import Pony
 
 
+handler = RotatingFileHandler('app/logs/app.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+
 def create_app(config_path='config.ProductionConfig'):
     app = Flask(__name__)
     app.config.from_object(config_path)
+
+    app.logger.addHandler(handler)
 
     bootstrap = Bootstrap(app)
     # extensions
