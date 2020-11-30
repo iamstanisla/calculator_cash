a779fd69a1ce44fc06bf902e6d8f0b91ed3acfd3
iamstanisla <goduyko08@mail.ru>
2020-11-30 21:56:58 +0300

    add a new fields in entity database and function hashing password. Add corresponding code in view function login and register for hashing password.

--- app/users/controllers.py
+++ app/users/controllers.py
@@ -18,6 +18,7 @@ from flask import redirect
 from flask import current_app
 from flask import request
 from flask import abort
+from flask import session
 from flask_login import login_user
 from flask_login import login_required
 from flask_login import logout_user
@@ -39,20 +40,20 @@ def login():
         username: str = login_form.username.data
         password: str = login_form.password.data
 
-        possible_user: Union[User, None] = User.get(username=username, password=password)
-        if not possible_user:
-            flash('Wrong username or password!', 'error')
+        possible_user: Union[User, None] = User.get(username=username)
+        if possible_user is None:
+            flash(f'User with username \"{username}\" does not exist', 'error')
             return render_template('users/login.html', login_form=login_form)
-        elif possible_user.password == password:
+        elif possible_user.check_hash_password(password):
             possible_user.last_login = datetime.now()
             login_user(possible_user)
             flash('Authorization was successful!')
-            current_app.logger.info(f'User \'{possible_user.username}\' was logged successfully! ')
+            current_app.logger.info(f'User \'{possible_user.username}\' was logged. ')
             return redirect(url_for('homepage'))
         else:
             # current_app.logger.warning('User')
-            flash('Wrong username or password!', 'error')
-            return redirect(url_for('.login'), login_form=login_form)
+            flash('Wrong password!', 'error')
+            return render_template('users/login.html', login_form=login_form)
     else:
         return render_template('users/login.html', login_form=login_form)
 
@@ -61,6 +62,11 @@ def login():
 @login_required
 def logout():
     logout_user()
+    # user_id = session.get('_user_id')
+    # print(session)
+    # user = User.get(id=user_id)
+    # print(user)
+    # current_app.logger.info(f'User \'{user.username} was logout.\'')
     return redirect(url_for('homepage'))
 
 
@@ -68,26 +74,39 @@ def logout():
 def register():
     reg_form = RegisterForm(meta={'csrf': False})
     if reg_form.validate_on_submit():
-        username = reg_form.username.data
-        password = reg_form.password.data
-        first_name = reg_form.first_name.data
-        second_name = reg_form.second_name.data
-        email = reg_form.email.data
+        username: str = reg_form.username.data
+        password: str = reg_form.password.data
+        first_name: str = reg_form.first_name.data
+        second_name: str = reg_form.second_name.data
+        email: str = reg_form.email.data
 
-        exist = db.User.get(username=username)
-        if exist:
-            flash('Username %s is already taken, choose another one' % username)
-            current_app.logger.info(f'username {username} already exit.')
+        exit_username: Union[User, None] = User.get(username=username)
+        exit_email: Union[User, None] = User.get(email=email)
+        if exit_username:
+            flash(f'Username \'{username}\' is already taken, choose another one')
+            current_app.logger.warning(f'trying to use an existing username \'{username}\'')
+            return redirect(url_for('.register'))
+        elif exit_email:
+            flash(f'email \'{email}\' is already taken, choose another one')
+            current_app.logger.warning(f'trying to use an existing email \'{email}\'')
             return redirect(url_for('.register'))
         else:
-            user = db.User(username=username, password=password)
-            user.last_login = datetime.now()
-            current_app.logger.info(f'add a new user {user.username}.')
+            # create instance of User
+            user = User(
+                username=username,
+                password=password,
+                first_name=first_name,
+                second_name=second_name,
+                email=email,
+                last_login=datetime.now()
+                )
+            user.set_hash_password(password)
+
+            current_app.logger.info(f'add a new user \'{user.username}\'.')
             flush()
             login_user(user)
-            current_app.logger.info(f'user {user.username} logged in {user.last_login}.')
+            current_app.logger.info(f'user \'{user.username}\' logged in {user.last_login}.')
             flash('Successfully registered')
             return redirect(url_for('homepage'))
     else:
-        # current_app.logger.info(f'error in time logining. error log: -> {reg_form.errors}.')
         return render_template('users/register.html', contact=reg_form)
--- app/users/models.py
+++ app/users/models.py
@@ -1,7 +1,11 @@
-from flask_login import LoginManager
-from pony.orm import Database, Required, Optional
-from flask_login import UserMixin
 from datetime import datetime
+
+
+from flask_login import LoginManager
+from flask_login import UserMixin
+from pony.orm import Database, Required, Optional, PrimaryKey
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 
 
 db = Database()
@@ -9,10 +13,28 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 
 
-class User(db.Entity, UserMixin):
-    username = Required(str, unique=True)
-    password = Required(str)
-    last_login = Optional(datetime)
+class User(UserMixin, db.Entity):
+    username=Required(str, unique=True)
+    email=Required(str, unique=True)
+    first_name=Required(str)
+    second_name=Required(str)
+    password=Required(str)
+    last_login=Optional(datetime)
+
+    def set_hash_password(self, password):
+        self.password=generate_password_hash(
+            password,
+            method='sha256'
+        )
+
+    def check_hash_password(self, password):
+        return check_password_hash(
+            self.password,
+            password
+        )
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 
 @login_manager.user_loader
