ea94abbd313818769789162e0ff5cdb989a3d61e
iamstanisla <goduyko08@mail.ru>
2020-12-02 10:56:51 +0300

    Add function view for upload a user image in server.

---
+++ app/collection/controllers.py
@@ -0,0 +1,21 @@
+from app.collection.form import UploadUserImage
+
+
+import os
+
+
+from flask import Blueprint
+from flask import current_app
+from werkzeug.utils import secure_filename
+
+
+collect = Blueprint('collection', __name__)
+
+
+@collect.route('/upload', method=['GET','POST'])
+def upload():
+	upload_form = UploadUserImage(meta={'csrf': False})
+	if upload_form.validate_on_submite():
+		filename: str = secure_filename(form.fileName.file.filename)
+		file_path: str = os.path.join(current_app.config('UPLOAD_FOLDER'), filename)
+		upload_form.fileName.file.save(file_path)
